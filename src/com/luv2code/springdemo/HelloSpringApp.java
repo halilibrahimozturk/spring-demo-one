b04f4e25e441e804220f5e0fc7e8c7000aacadc8
veli.adin <000972//Telli>
2022-05-12 15:39:42 +0300

    12.05.2022 15.39

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+    public static void main(String[] args){
+
+        //load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //retrieve bean rom spring container
+        Coach theCoach = context.getBean("myCoach",Coach.class);
+
+        //call methods n the bean
+        System.out.println(theCoach.getDailyWorkout());
+
+        //lets call our new method for fortunes
+        System.out.println(theCoach.getDailyFortune());
+
+        //close the context
+        context.close();
+    }
+}
