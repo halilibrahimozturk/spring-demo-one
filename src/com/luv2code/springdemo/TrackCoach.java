79009f1a40c90d2c5700ac5b1b4c186f28b6d75c
ibrahim <halilibraahimozturk@gmail.com>
2022-05-15 12:11:38 +0300

    15.05.2022 12.10

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -23,4 +23,20 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "just do it:" + fortuneService.getFortune();
     }
+
+
+    //add an init method
+    public void doMyStartupStuff() {
+
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+
+    //add an destroy method
+    public void doMyCleanupStuff() {
+
+        System.out.println("TrackCoach: inside method doMyCleanupStuff");
+    }
+
+
 }
